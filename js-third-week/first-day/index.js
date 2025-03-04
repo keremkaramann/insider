aa4dbfbe4a0e9e74688b079c42b878e57a27564b
Kerem Karaman <keremkaraman91@gmail.com>
2025-03-04 14:20:05 +0300

    first day task is done

---
+++ js-third-week/first-day/index.js
@@ -0,0 +1,83 @@
+$(function () {
+  // DOM hazır olduğunda çalışacak kodlar
+
+  const studentData = [
+    {
+      name: "Ali",
+      grade: 100,
+      course: "Yazılım",
+    },
+    {
+      name: "Ayşe",
+      grade: 90,
+      course: "Math",
+    },
+    {
+      name: "Joe",
+      grade: 30,
+      course: "Jquery",
+    },
+  ];
+
+  /* input focus */
+  $(".inputArea input").focus(function () {
+    $(this).css({
+      "border-color": "grey",
+      "background-color": "rgba(36, 117, 250, 0.3)",
+      transform: "scale(1.05)",
+    });
+  });
+
+  $(".inputArea input").blur(function () {
+    $(this).css({
+      "border-color": "grey",
+      "background-color": "rgba(202, 2, 2, 0.3)",
+      transform: "scale(1)",
+    });
+  });
+
+  function updateTable() {
+    $("#students tbody").empty();
+
+    studentData.forEach(function (student) {
+      const row = `
+            <tr>
+                <td>${student.name}</td>
+                <td>${student.course}</td>
+                <td>${student.grade}</td>
+            </tr>
+        `;
+      $("#students tbody").append(row);
+    });
+  }
+  updateTable();
+
+  $("#formBtn").click(function () {
+    const studentName = $("#studentName").val();
+    const courseName = $("#studentCourse").val();
+    const studentGrade = $("#studentGrade").val();
+
+    if (studentName && courseName && studentGrade) {
+      const newStudent = {
+        name: studentName,
+        course: courseName,
+        grade: studentGrade,
+      };
+
+      studentData.push(newStudent);
+
+      updateTable();
+
+      //clear inputs
+      $("#studentName").val("");
+      $("#studentCourse").val("");
+      $("#studentGrade").val("");
+    } else {
+      alert("Lütfen Tüm Alanları Doldurun");
+
+      $("#studentName").val("");
+      $("#studentCourse").val("");
+      $("#studentGrade").val("");
+    }
+  });
+});
