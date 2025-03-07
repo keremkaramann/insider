26f1ab96c9a910138ff14afd3af682ff66a33c82
Kerem Karaman <keremkaraman91@gmail.com>
2025-03-07 16:53:35 +0300

    shake eklendi

---
+++ js-third-week/third-day/index.js
@@ -0,0 +1,36 @@
+$(function () {
+  let cards = [];
+  function fetchUsers() {
+    $.get("https://randomuser.me/api/?results=5")
+      .done(function (data) {
+        data.results.forEach((user) => {
+          let newCard = $(`
+              <div class="card">
+              <div>
+              <img src=${user.picture.medium} alt=""/>
+              </div>
+              <div>
+              <p>Name: ${user.name.title}.${user.name.last}</p>
+             <p>Origin: ${user.location.country}</p>
+             <p>Email: ${user.email}</p>
+             <button class="shakeButton">Shake Efekti Uygula</button>
+              </div>
+          </div>
+            `);
+          cards.push(newCard);
+        });
+        $(".card_container").append(cards);
+        $(".card").fadeIn(1000);
+      })
+      .fail(function () {
+        alert("API ERROR!");
+      });
+  }
+
+  fetchUsers();
+
+  //shake func
+  $(".card_container").on("click", ".shakeButton", function () {
+    $(this).closest(".card").effect("shake", { times: 3, distance: 10 }, 500);
+  });
+});
